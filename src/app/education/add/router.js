cec9b81ac084d68442a01e7902fe7436d8d80e49
woori <dlsltlvmfl@naver.com>
2024-08-02 23:45:39 +0900

    initial build ...

---
+++ src/app/education/add/router.js
@@ -0,0 +1,31 @@
+import connectToDB from "@/database";
+import Education from "@/models/Education";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req) {
+  try {
+    await connectToDB();
+    const extractData = await req.json();
+    const saveData = await Education.create(extractData);
+
+    if (saveData) {
+      return NextResponse.json({
+        success: true,
+        message: "데이터가 성공적으로 저장되었습니다.",
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "오류가 발견되었습니다. 다시 시도해 주세요.",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      success: false,
+      message: "오류가 발견되었습니다. 다시 시도해 주세요.",
+    });
+  }
+}
